7dcbadbe5253cf5bcd7700800f85ee3ad3df3cfd
Ravi Rathore <ravipratapsinghrathore15@gmail.com>
2019-07-16 18:01:08 +1000

    added add and remove methods to ShoppingCart.java

--- ShoppingCart.java
+++ ShoppingCart.java
@@ -29,9 +29,35 @@ public class ShoppingCart
     {
         return userID;
     }
-
+    //return list of products
     public ArrayList<Product> getProducts()
     {
         return products;
+    }
+
+    // check if product exists in the shopping cart
+    // return -1 if not found; else index
+    public int contains(String productID) {
+        for (int i = 0; i < products.size(); i++) {
+            if (products.get(i).getID().equals(productID)) {
+                return i;
+            }
+        }
+        return -1;
+    }
+    // method to add a product to the product list
+    public void addProduct(String productID, String description) {
+        products.add(new Product(productID, description));
+    }
+
+    //remove the first product with given id if it is added to the cart
+    public void removeProduct(String productID) {
+        //check if product is in the cart
+        int index = contains(productID);
+        if(index >= 0)
+        {
+            products.remove(index);
+        }
+
     }
 }
